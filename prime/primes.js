9863e3858246f87faabc631e26acc32a2cca4f0b
Valentin Slawicek <valentin.slawi@gmail.com>
2024-04-11 10:46:46 +0200

    Test JS with both node+bun

--- prime/primes.js
+++ prime/primes.js
@@ -6,7 +6,7 @@ function calculatePrimes() {
             count++;
         }
     }
-    console.log(`JavaScript:\tThere are ${count} prime numbers between 1 and 10_000_000. It took ${(Date.now() - start) / 1000} s.`);
+    console.log(`${process.argv[2]}: \tThere are ${count} prime numbers between 1 and 10_000_000. It took ${(Date.now() - start) / 1000} s.`);
 }
 
 function isPrime(n) {
